baba45bc3c71a2bd8f8d6d0a2efe588c811732a6
Eduardo da Silva <eaugusto@outlook.com>
2024-03-19 21:26:50 +0100

    externalizando variáveis.

---
+++ k8s/terraform/terraform.tfvars
@@ -0,0 +1,24 @@
+region                              = "us-west-2"
+environment                         = "dev"
+author                              = "ProInsightTech"
+profile                             = "default"
+
+//IAM
+account_id                          = "279044386247"
+user_name                           = "lanchonete-api"
+role_name                           = "lanchonete-api-role"
+
+// EKS
+cluster_name                        = "snackshop-cluster"
+cluster_version                     = "1.28"
+cluster_endpoint_public_access      = true
+
+// VPC
+vpc_id                              = "vpc-03ff6e3c383770fe7"
+subnet_ids                          = ["subnet-03a5221550ebcaa7a", "subnet-093d549666b80aeb6", "subnet-0ddb10d62fe5cc33a"]
+
+// Node Group EC2
+instance_type_1                     = "t3a.medium"
+instance_type_2                     = "t3.medium"
+instance_type_3                     = "t3a.large"
+instance_capacity_type              = "SPOT"
---
+++ k8s/terraform/variables.tf
@@ -0,0 +1,122 @@
+// *************************************** //
+// ******** Environment Variables ******** // 
+// *************************************** //
+
+//Region
+variable "region" {
+  description = "AWS Region"
+  type        = string
+  default     = "us-west-2"
+}
+
+//Environment
+variable "environment" {
+  description = "Environment"
+  type = string
+  default = "dev"
+}
+
+//Author
+variable "author" {
+  description = "Author"
+  type        = string
+  default     = "ProInsightTech"
+}
+
+// *************************************** //
+// ********** Account Variables ********** //
+// *************************************** //
+
+//AWS Profile
+variable "profile" {
+  description = "AWS Profile"
+  type = string
+  default     = "default"
+}
+
+//AWS Account ID
+variable "account_id" {
+  description = "AWS Account ID"
+  type        = string
+}
+
+//AWS User Name
+variable "user_name" {
+  description = "AWS User Name"
+  type        = string
+}
+
+variable "role_name" {
+  description = "AWS Role Name"
+  type        = string
+}
+
+
+// *************************************** //
+// ******** EKS Cluster Variables ******** // 
+// *************************************** //
+
+//AWS EKS Cluster Name
+variable "cluster_name" {
+  description = "EKS Cluster Name"
+  type        = string
+}
+
+//AWS EKS Cluster Version
+variable "cluster_version" {
+  description = "EKS Cluster Version"
+  type        = string
+  default     = "1.27"
+}
+
+//AWS EKS Cluster Endpoint Public Access
+variable "cluster_endpoint_public_access" {
+  description = "EKS Cluster Endpoint Public Access"
+  type        = bool
+  default     = true
+}
+
+//AWS EKS Node Group Instance Type
+variable "instance_type_1" {
+  description = "EKS Node Group Instance Type 1"
+  type        = string
+  default     = "t3a.medium"
+}
+
+//AWS EKS Node Group Instance Type
+variable "instance_type_2" {
+  description = "EKS Node Group Instance Type 2"
+  type        = string
+  default     = "t3.medium"
+}
+
+//AWS EKS Node Group Instance Type
+variable "instance_type_3" {
+  description = "EKS Node Group Instance Type 3"
+  type        = string
+  default     = "t3a.large"
+}
+
+
+variable "instance_capacity_type" {
+  description = "EKS Node Group Instance Capacity Type"
+  type        = string
+  default     = "SPOT"
+}
+
+// *************************************** //
+// ******** VPC & Subnet Variables ******* //
+// *************************************** //
+
+//AWS VPC ID
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+//AWS Subnet IDs
+variable "subnet_ids" {
+  description = "Subnet IDs"
+  type        = list(string)
+}
+
